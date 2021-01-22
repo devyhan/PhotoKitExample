6d3d9e2110119f8b388d6e25fb0d8762b67f7171
devyhan93 <devyhan93@gmail.com>
2021-01-22 09:44:04 +0900

    init project

---
+++ PhotoKitExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoKitExample
+//
+//  Created by 요한 on 2021/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhotoKitExample/PhotoKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoKitExampleApp.swift
+//  PhotoKitExample
+//
+//  Created by 요한 on 2021/01/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
